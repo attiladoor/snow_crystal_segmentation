Fix data normalization issue

model/dataset.py
@@ -17,22 +17,20 @@ class DataPaths:
 
 
 def get_data_paths(base_dir, split_ratio=0.15):
-
     total_input_img_paths = []
     total_target_img_paths = []
 
     for f in FOLDERS:
-
         dataset_dir = os.path.join(base_dir, f)
 
-        input_dir = os.path.join(dataset_dir, "cropped_originals")
+        input_dir = os.path.join(dataset_dir, "cropped_original_png")
         target_dir = os.path.join(dataset_dir, "cropped_contours")
 
         input_img_paths = sorted(
             [
                 os.path.join(input_dir, fname)
                 for fname in os.listdir(input_dir)
-                if fname.endswith(".tif")
+                if fname.endswith(".png")
             ]
         )
         target_img_paths = sorted(
@@ -113,30 +111,30 @@ class CropsDataset(keras.utils.Sequence):
             input_img = load_img(
                 input_path, target_size=self.img_size, color_mode="grayscale"
             )
-
-            input_img_np_norm = np.array(input_img, dtype=np.float32) / 255.0
-            x[j] = input_img_np_norm
+            input_img_np_norm_gray = np.array(input_img).astype(np.float32)
+            h, w = input_img_np_norm_gray.shape
+            x[j, 0:h, 0:w] = (
+                np.expand_dims(input_img_np_norm_gray[:, :], axis=2) / 65535.0
+            )  # normalize 16bit data
 
         y = np.zeros((self.batch_size,) + self.img_size, dtype="uint8")
         for j, path in enumerate(batch_target_img_paths):
             img = load_img(path, target_size=self.img_size, color_mode="grayscale")
-            np_img = np.array(img, dtype=np.uint8) / 255.0
-            y[j] = np_img
+            np_img = np.array(img, dtype=np.uint8) / 255
+            h, w = np_img.shape
+            y[j, 0:h, 0:w] = 1 - np_img
 
         return self.do_augmentation(x, y)
 
     @tf.function
     def do_augmentation(self, input_image, input_mask):
-
         input_mask = tf.expand_dims(input_mask, axis=3)
 
         if self.do_augment:
-
             CROP_H = 864
             CROP_W = 1152
 
             if tf.random.uniform(()) > 0.5:
-
                 offset_height = int((self.img_size[0] - CROP_H) * tf.random.uniform(()))
                 offset_width = int((self.img_size[1] - CROP_W) * tf.random.uniform(()))
 

model/train.py
@@ -26,7 +26,6 @@ epochs = 4
 
 
 def main(args):
-
     # Free up RAM in case the model definition cells were run multiple times
     keras.backend.clear_session()
 
@@ -34,7 +33,7 @@ def main(args):
     # model = get_model(img_size, num_classes)
     model = sm.Linknet(
         "resnet18",
-        input_shape=(model_size[0], model_size[1], 4),
+        input_shape=(model_size[0], model_size[1], 1),
         classes=num_classes,
         activation="sigmoid",
         encoder_weights=None,
@@ -99,7 +98,7 @@ def main(args):
             "Validation images", os.path.join(args.output_folder, "logs"), val_gen[0]
         ),
         OverlayWriter(
-            "Train images", os.path.join(args.output_folder, "logs"), train_gen[12]
+            "Train images", os.path.join(args.output_folder, "logs"), train_gen[0]
         ),
         keras.callbacks.LearningRateScheduler(scheduler),
     ]
@@ -110,7 +109,7 @@ def main(args):
     model.save(os.path.join(args.output_folder, "model_file"))
     input_signature = (
         tf.TensorSpec(
-            (None, model_size[0], model_size[1], 4),
+            (None, model_size[0], model_size[1], 1),
             tf.float32,
             name="image_input",
         ),

model/writer.py
@@ -13,7 +13,6 @@ class OverlayWriter(keras.callbacks.Callback):
         self._create_writer()
 
     def _create_writer(self):
-
         self.writer = tf.summary.create_file_writer(
             self.logdir,
             max_queue=None,
@@ -24,17 +23,19 @@ class OverlayWriter(keras.callbacks.Callback):
         )
 
     def on_epoch_end(self, epoch, logs=None):
-
-        predictions_multi_channel = self.model.predict(self.samples[0])
+        predictions = self.model.predict(self.samples[0])
         samples_float = self.samples[0]
 
+        sample_3ch = tf.image.grayscale_to_rgb(samples_float, name=None)
+
         red_ch = tf.where(
-            predictions_multi_channel[:, :, :, 0] > 0.5,
-            tf.ones_like(predictions_multi_channel[:, :, :, 0]),
-            samples_float[:, :, :, 0],
+            predictions[:, :, :, 0] > 0.5,
+            tf.ones_like(predictions[:, :, :, 0]),
+            sample_3ch[:, :, :, 0],
         )
+
         samples_overlay = tf.stack(
-            [red_ch, samples_float[:, :, :, 1], samples_float[:, :, :, 2]], axis=3
+            [red_ch, sample_3ch[:, :, :, 1], sample_3ch[:, :, :, 2]], axis=3
         )
 
         with self.writer.as_default():
@@ -43,14 +44,14 @@ class OverlayWriter(keras.callbacks.Callback):
         red_ch_annot = tf.where(
             self.samples[1][:, :, :, 0] > 0,
             tf.cast(self.samples[1][:, :, :, 0], tf.float32),
-            samples_float[:, :, :, 0],
+            sample_3ch[:, :, :, 0],
         )
 
         samples_overlay_gt = tf.stack(
             [
                 red_ch_annot,
-                samples_float[:, :, :, 1],
-                samples_float[:, :, :, 2],
+                sample_3ch[:, :, :, 1],
+                sample_3ch[:, :, :, 2],
             ],
             axis=3,
         )