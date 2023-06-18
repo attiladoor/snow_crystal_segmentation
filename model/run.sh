8b23eabe444981408357d3e64b8305c309a5b0cf
Attila Door <attiladoor@gmail.com>
2023-06-18 18:01:31 +0200

    Cache data before training

--- model/run.sh
+++ model/run.sh
@@ -1,6 +1,10 @@
 xhost +
 
 DATA_FOLDER="$PWD/data/cropped_/"
+TMP_DATA_FOLDER="/tmp/dataset/$DATA_FOLDER"
+
+mkdir -p $TMP_DATA_FOLDER
+rsync -a --info=progress2 -u  $DATA_FOLDER/ $TMP_DATA_FOLDER #--delete
 
 docker  run \
         -it \
@@ -9,11 +13,9 @@ docker  run \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         --gpus all \
         -v $PWD:$PWD \
-        -v $DATA_FOLDER:$DATA_FOLDER \
+        -v $TMP_DATA_FOLDER:$TMP_DATA_FOLDER \
         -p 8888:8888 \
-        -p 6006:6006 \
         --name trainer \
         --rm \
-        cuda-tensorflow /bin/bash -c "python3 ${PWD}/model/train.py --data_folder $DATA_FOLDER --output_folder $PWD/training && \
-        tensorboard --logdir_spec $PWD/training"
+        cuda-tensorflow /bin/bash -c "python3 ${PWD}/model/train.py --data_folder $TMP_DATA_FOLDER --output_folder $PWD/out/training"
 
