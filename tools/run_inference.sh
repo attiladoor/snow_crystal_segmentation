bb05ea8329fa10cb8480ea55e68068aa1111e621
János <faszalegeny@gmail.com>
2024-02-16 14:50:21 +0100

    yes

--- tools/run_inference.sh
+++ tools/run_inference.sh
@@ -6,6 +6,6 @@ docker  run \
     --gpus all \
     -v $PWD:$PWD \
     cuda-tensorflow /bin/bash -c "python3 $REPO_ROOT/tools/run_inference.py \
-        --model $REPO_ROOT/out/training_06_augmented/model.onnx \
-        --input_folder $REPO_ROOT/data/cropped_/batch_1/cropped_original_png \
-        --output_folder $REPO_ROOT/data/cropped_/batch_1/out"
+        --model $REPO_ROOT/models/m20/model.onnx \
+        --input_folder $REPO_ROOT/test_png \
+        --output_folder $REPO_ROOT/scs_out/m20 "
